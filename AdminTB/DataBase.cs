71c01fda32e30ab2c0b3f2b045fa2841982ea64c
Admira1B <Vreizenbuk@mail.ru>
2022-05-16 17:18:58 +0700

    first commit

---
+++ AdminTB/DataBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AdminTB
+{
+    class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source = 10.0.22.5; Initial Catalog = ARM_TB; User ID = ARM_TB; Password = 1234ABCd");
+
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
